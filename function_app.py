824df18798134516ff8d0e854677fd701f2aad47
mfoster58 <mathfoster@microsoft.com>
2025-02-19 17:57:44 -0800

    moving function to new file

--- function_app.py
+++ function_app.py
@@ -1,1 +1,6 @@
 import repost_trigger.codexreposter
+import azure.functions as func
+app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+
+# Register the functions from codexreposter
+repost_trigger.codexreposter.register_functions(app)
